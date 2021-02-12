50c88529038d652a62f22705dcd2388836b8d644
analyticone <steven.kreutzer@yahoo.com>
2021-02-12 17:54:23 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.weather.Weather.Weather;
+
+import org.json.JSONException;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    Weather weather;
+
+    String url = "https://api.openweathermap.org/data/2.5/weather?zip=07090&units=imperial&appid=4c0b04940376050ab9ff85700fad3570";
+    public static final String TAG = "MainActivity";
+
+    TextView tvName;
+    TextView tvTemp;
+    TextView tvWind;
+    TextView tvDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvName = findViewById(R.id.tvName);
+        tvTemp = findViewById(R.id.tvTemp);
+        tvWind = findViewById(R.id.tvWind);
+        tvDescription = findViewById(R.id.tvDescription);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, json.toString());
+
+                try {
+                    weather = new Weather(json.jsonObject);
+                    Log.d(TAG, "Weather: " + weather);
+
+                    tvName.setText(weather.getName());
+                    tvTemp.setText("" + weather.getTemp() +" F");
+                    tvWind.setText("" + weather.getWind() + " MPH");
+                    tvDescription.setText(weather.getDescription());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/weather/Weather/Weather.java
@@ -0,0 +1,53 @@
+package com.example.weather.Weather;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Weather
+{
+    String name;
+    double temp;
+    double feels_like;
+    String description;
+    double wind;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public double getFeels_like() {
+        return feels_like;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getWind() {
+        return wind;
+    }
+
+    public Weather(JSONObject jsonObject) throws JSONException {
+        name = jsonObject.getString("name");
+        JSONObject main = jsonObject.getJSONObject("main");
+        temp = main.getDouble("temp");
+        feels_like = main.getDouble(("feels_like"));
+        description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+        wind = jsonObject.getJSONObject("wind").getDouble("speed");
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "name='" + name + '\'' +
+                ", temp=" + temp +
+                ", feels_like=" + feels_like +
+                ", description='" + description + '\'' +
+                ", wind=" + wind +
+                '}';
+    }
+}
